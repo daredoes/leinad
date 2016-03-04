f77b6fb3a2c11500ed315d9ff9b9b5525b338799
Daniel Evans <me@danielarevans.com>
2016-03-04 10:20:25 -0800

    remember improvements

--- memory.py
+++ memory.py
@@ -15,6 +15,14 @@ rem = "\\bremember\\b %s \\bis\\b %s" % (till_white, till_end)
 @respond_to(rem, re.IGNORECASE)
 def remember(message, key, note):
     if message.is_approved("any"):
+        temp = note.split(" ")
+        temp2 = ""
+        for x in temp:
+            if x[0] == "<"  and x[len(x)-1] == ">":
+                temp2 += " " + (x.strip("><"))
+            else:
+                temp2 += " " + (x)
+        note = temp2.strip(" ")
         if db.mem.count({"key":key}) == 0:
             db.mem.insert_one(memory_dict(key, note))
             message.send("I'll be sure to remember that.")
@@ -32,3 +40,14 @@ def what(message, key):
                 message.send(x['note'])
         else:
             message.send("I don't know what %s is" % key)
+
+fer = '\\bforget what %s is' % till_white
+@listen_to(fer, re.IGNORECASE)
+@respond_to(fer, re.IGNORECASE)
+def forget(message, key):
+    if message.is_approved("admin"):
+        if db.mem.count({"key":key}) != 0:
+            db.mem.delete_many({"key": key})
+            message.send("I have forgotten what %s is" % key)
+        else:
+            message.send("I don't even know what %s is in the first place." % key)
